cf0110bbfc30ba1ec03926679d8bda3837137663
nokcharathae <rkwmal0000@naver.com>
2021-12-23 20:33:58 +0900

    doing

--- 4_Queue2.py
+++ 4_Queue2.py
@@ -7,3 +7,38 @@
 # empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
 # front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 # back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+num = (int)(input())
+q_list = [input().split() for _ in range(num)]
+myqueue = []
+
+for i in range(num):
+    if q_list[i][0] == "push" :
+        myqueue.append(q_list[i][1])
+    
+    elif q_list[i][0] == "pop" :
+        if len(myqueue) == 0:
+            print(-1)
+        else: 
+            print(myqueue.pop(0)) 
+    
+    elif q_list[i][0] == "size" :
+        print(len(myqueue))
+
+    elif q_list[i][0] == "empty" :
+        if len(myqueue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif q_list[i][0] == "front" :
+        if len(myqueue) == 0:
+            print(-1)
+        else: 
+            print(myqueue[0])
+
+    elif q_list[i][0] == "back" :
+        if len(myqueue) == 0:
+            print(-1)
+        else: 
+            print(myqueue[-1])
